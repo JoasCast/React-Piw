ff1bd503f19dbf1c93879c2997abf37f92e4870d
JoasCast <74987175+JoasCast@users.noreply.github.com>
2022-06-12 14:12:59 -0300

    Create posts.view.js

---
+++ atividade_2/app/views/posts.view.js
@@ -0,0 +1,23 @@
+// module.exports.render = function(post){
+//     return{
+//         id: post._id,
+//         texto: post.trexto,
+//         like: post.like,
+
+//     }
+// }
+
+function render(post) {
+  return {
+    id: post._id,
+    texto: post.texto,
+    likes: post.likes,
+    id_usuario: post.id_usuario,
+  };
+}
+module.exports.render = render;
+
+function renderMany(posts) {
+  return posts.map(render);
+}
+module.exports.renderMany = renderMany;
