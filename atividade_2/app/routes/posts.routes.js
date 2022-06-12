98e2143e06ea695d4d6d33691217316db6970342
JoasCast <74987175+JoasCast@users.noreply.github.com>
2022-06-12 14:12:52 -0300

    Create posts.routes.js

---
+++ atividade_2/app/routes/posts.routes.js
@@ -0,0 +1,9 @@
+let controller = require("../controllers/posts.controller");
+
+module.exports = function (app) {
+  app.get("/posts", controller.listaPosts);
+  app.get("/posts/:id", controller.obterPost);
+  app.get("/posts/:id/comentarios", controller.buscarComentariosDoPost);
+  app.post("/posts", controller.adcionarPost);
+  app.delete("/posts/:id", controller.removerPost);
+};
